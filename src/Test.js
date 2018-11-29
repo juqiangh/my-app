e18fc99be399e84127f94c7ba82f86e7c379ed43
huangjiacong <249364174@qq.com>
2018-11-29 16:03:26 +0800

    add Test.js

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+
+class Test extends Component {
+    // 当父组件的render函数被运行时，它的子组件的render都将被重新执行一次
+    render () {
+        console.log('Test render');
+        return (
+            <div>{this.props.content}</div>
+        )
+    }
+}
+
+export default Test;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import TodoItem from './TodoItem';
+import Test from  './Test';
 import './style.css';
 
 class TodoList extends Component {
 
     constructor(props) {
         super(props);
+        // 当组件的state或者props发生改变的时候，render函数就会重新执行
         this.state = {
             inputValue: '',
             list: []
@@ -32,6 +34,7 @@ class TodoList extends Component {
                 <ul>
                     {this.getTodoItem()}
                 </ul>
+                <Test content={this.state.inputValue}/>
             </Fragment>
         );
     }
