597e2d598dc7c4c68e7a41fc66980cf0fc9d7568
juqiangh <18296154764@163.com>
2018-11-29 23:27:04 +0800

    react的生命周期

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
 
@@ -8,6 +9,7 @@ class TodoItem extends Component {
     }
 
     render() {
+        console.log('child render');
         const { content } = this.props ;
         // JSX -> JS对象 -> 真实DOM
         return (
@@ -19,10 +21,41 @@ class TodoItem extends Component {
         return React.createElement('div', {}, React.createElement('span', {}, 'item'));*/
     }
 
+    //一个组件要从父组件接受参数
+    //只要父组件的render函数被重新执行了，子组件的生命周期函数就会被执行
+    //如果这个组件第一次存在于父组件中，不会执行
+    //如果这个组件之前已经存在于父组件中，才会执行
+    /*componentWillReceiveProps(nextProps, nextContext) {
+        console.log('child componentWillReceiveProps');
+    }*/
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        if(nextProps.content !== this.props.content) {
+            return true;
+        }
+        return false;
+    }
+
+    //当这个组件即将被从页面中剔除的时候，会被执行
+    /*componentWillUnmount() {
+        console.log('child componentWillUnmount');
+    }*/
+
     handleClick() {
         const { deleteItem, index} = this.props;
         deleteItem(index);
     }
 }
+
+TodoItem.propTypes = {
+    /*test: PropTypes.string.isRequired,*/
+    content: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+}
+
+/*TodoItem.defaultProps = {
+    test: 'hello world'
+}*/
 
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import TodoItem from './TodoItem';
+import axios from 'axios';
 /*import Test from  './Test';*/
 import './style.css';
 
@@ -17,35 +18,54 @@ class TodoList extends Component {
         this.handleItemDelete = this.handleItemDelete.bind(this);
     }
 
-    componentWillMount() {
+    //在组件即将被挂载到页面的时刻自动执行
+    /*componentWillMount() {
         console.log('componentWillMount');
-    }
+    }*/
 
+    //组件被挂载到页面之后,自动执行
     componentDidMount() {
         console.log('componentDidMount');
+        axios.get('/api/todolist').then(() => {
+            alert('succ');
+        }).catch(() => {
+            alert('error');
+        });
     }
 
-    componentWillReceiveProps(nextProps, nextContext) {
+    //一个组件要从父组件接受参数
+    //只要父组件的render函数被重新执行了，子组件的生命周期函数就会被执行
+    //如果这个组件第一次存在于父组件中，不会执行
+    //如果这个组件之前已经存在于父组件中，才会执行
+    /*componentWillReceiveProps(nextProps, nextContext) {
         console.log('componentWillReceiveProps');
-    }
+    }*/
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
+    // 组件被更新之前，他会自动执行
+    /*shouldComponentUpdate(nextProps, nextState, nextContext) {
         console.log('shouldComponentUpdate');
-    }
+        return true;
+    }*/
 
-    componentWillUpdate(nextProps, nextState, nextContext) {
+    //组件被更新之前，它会自动执行，但是他在shouldComponentUpdate之后执行
+    //如果shouldComponentUpdate返回true它才执行
+    //如果返回false，这个函数就不会被执行了
+    /*componentWillUpdate(nextProps, nextState, nextContext) {
         console.log('componentWillUpdate');
-    }
+    }*/
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
+    //组件更新完成之后，它会自动执行
+    /*componentDidUpdate(prevProps, prevState, snapshot) {
         console.log('componentDidUpdate');
-    }
+    }*/
 
-    componentWillUnmount() {
+    //当这个组件即将被从页面中剔除的时候，会被执行
+    /*componentWillUnmount() {
         console.log('componentWillUnmount');
-    }
+    }*/
 
     render() {
+        console.log('render');
         return (
             <Fragment>
                 <div>
@@ -75,7 +95,7 @@ class TodoList extends Component {
                         key = {index}
                         content={item}
                         index={index}
-                        deleteItem={this.handleItemDelete.bind(this)}
+                        deleteItem={this.handleItemDelete}
                     />
             )
             /*<li
